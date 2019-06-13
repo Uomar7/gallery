8b7f694af9b0ea49c1f5ba4918b55b2dc2abec74
Uomar <uomarearlie7@gmail.com>
2019-06-14 00:03:26 +0300

    created past images view function

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.images_of_the_day,name = 'imagesToday')
+    url('^today/$',views.images_of_the_day,name = 'imagesToday'),
+    url(r'^past/(\d{4}-\d{2}-\d{2})/$',views.past_images,name = 'pastImages')
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import datetime as dt
 
 def welcome(request):
@@ -11,6 +11,37 @@ def images_of_the_day(request):
         <html>
             <body>
                 <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday', "Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
+def past_images(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
