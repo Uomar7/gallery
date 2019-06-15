0ac2b42d867bec75511dcc286d8799ae0189365d
Uomar <uomarearlie7@gmail.com>
2019-06-15 18:27:10 +0300

    modified the views for past images

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url('^$',views.welcome,name = 'welcome'),
     url('^today/$',views.images_of_the_day,name = 'imagesToday'),
-    url(r'^past/(\d{4}-\d{2}-\d{2})/$',views.past_images,name = 'pastImages')
+    url(r'^past/(\d{4}-\d{2}-\d{2})/$',views.past_images,name = 'pastImages'),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
@@ -7,15 +7,22 @@ def welcome(request):
 
 def images_of_the_day(request):
     date = dt.date.today()
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+   
+    return render(request, 'all-out/today-pics.html', {"date":date})
 
+def past_images(request,past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+
+    if date == dt.date.today():
+        return redirect(images_of_the_day)
+
+    return render(request, 'all-out/past-pics.html', {"date":date})
 # def convert_dates(dates):
 
 #     # Function that gets the weekday number for the date.
@@ -27,21 +34,3 @@ def images_of_the_day(request):
 #     # Returning the actual day of the week
 #     day = days[day_number]
 #     return day
-
-def past_images(request,past_date):
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
