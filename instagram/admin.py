f2e639a844653c757d03c3bf710bad8652b31701
Uomar <uomarearlie7@gmail.com>
2019-06-17 15:34:15 +0300

    changed display for the landing page

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Image,category,Location
 
-admin.site.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('category',)
+
+
+admin.site.register(Image, ImageAdmin)
 admin.site.register(category)
 admin.site.register(Location)
