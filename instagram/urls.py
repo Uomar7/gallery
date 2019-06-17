fc3ccde19714a680cdbea161f6fb5d4b20ab4145
Uomar <uomarearlie7@gmail.com>
2019-06-17 13:29:34 +0300

    add url patterns to reference location of uploaded images

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     url(r'^past/(\d{4}-\d{2}-\d{2})/$',views.past_images,name = 'pastImages'),
     url(r'^search/', views.search_results, name = 'search_results'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(setting.MEDIA_URL, document_root = settings.MEDIA_ROOT)
