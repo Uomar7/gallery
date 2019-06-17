3218e094380646e26c41ed3caaae1e7eaeab10f4
Uomar <uomarearlie7@gmail.com>
2019-06-17 08:57:42 +0300

    created the posted field in the db.

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length = 40)
+    description = models.TextField()
+    email = models.EmailField()
+    posted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['name']
+
+class category(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    location_name = models.CharField(max_length = 40)
+    image = models.ForeignKey(Image)
+    category = models.ManyToManyField(category)
